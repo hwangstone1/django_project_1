6b568b45f6659dbd279281d34dd6e2cd977416d4
hwangstone1 <mylee553@naver.com>
2022-04-03 22:56:36 +0900

    wysiwyg

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,14 @@
 from django.forms import ModelForm
+from django import forms
 
 from articleapp.models import Article
 
 
 class ArticleCreationForm(ModelForm):
+
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'editable',
+                                                           'style':'height:auto; text-left;'}))
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
